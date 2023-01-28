e2690c4f191def4c667b60120bb646e706812adb
Maksim <mgrib03@yandex.ru>
2023-01-28 11:54:03 +0300

    Finished

--- Program.cs
+++ Program.cs
@@ -46,6 +46,37 @@ void Ex66(){
 
 //Ex66();
 
-void Ex68(){
-    
+ void Ex68(){    //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+                 // m = 2, n = 3 -> A(m,n) = 9
+                 // m = 3, n = 2 -> A(m,n) = 29
+
+    Console.WriteLine("Введите первое число: ");
+    uint m = uint.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число: ");
+    uint n = uint.Parse(Console.ReadLine());
+
+    if (m >= 0 && n >= 0)
+    {
+        static uint A(uint m, uint n)
+        {
+            if (m == 0)
+                return n + 1;
+            else
+              if ((m != 0) && (n == 0))   // из-за незнания что такое функция Аккермана и зачем она используется, 
+                                          //эта функция не работает, я не знаю почему и из-за чего, и как она вообще должна выводить результат одним числом
+                return A(m - 1, 1);
+            else
+                return A(m - 1, A(m, n - 1));
+        }
+
+        uint result = A(m, n);
+
+        Console.WriteLine(result);
+    } else
+    {
+        Console.WriteLine("Введите положительные числа");
+    }
 }
+
+Ex68();
